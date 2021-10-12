124e330e28bcc69af9114743c370281e3b5a0d33
Phambanam <Phambanam1999@gmail.com>
2021-10-12 21:04:05 +0300

    Lab2_1

---
+++ app/src/main/java/com/PhamBaNam/lab2/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.PhamBaNam.lab2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+const val  TAG: String = "my_log"
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d(TAG,"onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG,"onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG,"onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG,"onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG,"onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG,"Destroy")
+    }
+}
